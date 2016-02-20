7dcd8b4253670dd3a2bc5f83b577e3844e390dfd
Tomás Jacob <tomas.j.martins@ist.utl.pt>
2016-02-20 05:04:29 +0000

    PUTAS E VINHO VERDE

---
+++ studyat/server/studyat.js
@@ -0,0 +1,37 @@
+Tasks = new Mongo.Collection("tasks");
+ 
+if (Meteor.isClient) {
+  console.log("1")
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      // Show newest tasks at the top
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+  console.log("2")
+  Template.submit.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;     
+
+      // Insert a task into the collection
+      Tasks.insert({
+        nome: text,
+        createdAt: new Date() // current time
+      });     
+      console.log("3")
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
